7f1e0a56e440014530f0c71c2753553a3ba461df
ryota717 <ryota0717rocky@gmail.com>
2019-05-06 16:40:03 +0900

    first commit

---
+++ data/__init__.py
@@ -0,0 +1,6 @@
+# from .voc import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES
+from .voc0712 import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES
+from .coco import COCODetection
+from .objects365 import obj365Detection
+from .data_augment import *
+from .anchors import *
